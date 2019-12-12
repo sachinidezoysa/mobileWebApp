22f427dc9c5076212efff63eb840b8229cff2e37
shifan1996 <mohamed.2015035@iit.ac.lk>
2019-12-12 15:55:10 +0530

    product page enhancement

--- Exiri Mobile/www/js/product.js
+++ Exiri Mobile/www/js/product.js
@@ -150,6 +150,174 @@ var arrProducts = [{
         image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
         price: 178.00,
         discount: 0,
+        isFavourite: false,
+        rank: 4,
+        reviews: []
+    },
+    {
+        id: 1,
+        name: 'Adidas Tshirt',
+        decription: 'Adidas brand with certified material',
+        category: 'mobileAccesories',
+        image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
+        price: 178.00,
+        discount: 0,
+        isFavourite: false,
+        rank: 4,
+        reviews: []
+    },
+    {
+        id: 1,
+        name: 'Adidas Tshirt',
+        decription: 'Adidas brand with certified material',
+        category: 'mobileAccesories',
+        image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
+        price: 178.00,
+        discount: 0,
+        isFavourite: false,
+        rank: 4,
+        reviews: []
+    },
+    {
+        id: 1,
+        name: 'Adidas Tshirt',
+        decription: 'Adidas brand with certified material',
+        category: 'mobileAccesories',
+        image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
+        price: 178.00,
+        discount: 0,
+        isFavourite: false,
+        rank: 4,
+        reviews: []
+    },
+    {
+        id: 1,
+        name: 'Adidas Tshirt',
+        decription: 'Adidas brand with certified material',
+        category: 'mobileAccesories',
+        image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
+        price: 178.00,
+        discount: 0,
+        isFavourite: false,
+        rank: 4,
+        reviews: []
+    },
+    {
+        id: 1,
+        name: 'Adidas Tshirt',
+        decription: 'Adidas brand with certified material',
+        category: 'mobileAccesories',
+        image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
+        price: 178.00,
+        discount: 0,
+        isFavourite: false,
+        rank: 4,
+        reviews: []
+    },
+    {
+        id: 1,
+        name: 'Adidas Tshirt',
+        decription: 'Adidas brand with certified material',
+        category: 'mobileAccesories',
+        image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
+        price: 178.00,
+        discount: 0,
+        isFavourite: false,
+        rank: 4,
+        reviews: []
+    },
+    {
+        id: 1,
+        name: 'Adidas Tshirt',
+        decription: 'Adidas brand with certified material',
+        category: 'mobileAccesories',
+        image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
+        price: 178.00,
+        discount: 0,
+        isFavourite: false,
+        rank: 4,
+        reviews: []
+    },
+    {
+        id: 1,
+        name: 'Adidas Tshirt',
+        decription: 'Adidas brand with certified material',
+        category: 'mobileAccesories',
+        image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
+        price: 178.00,
+        discount: 0,
+        isFavourite: false,
+        rank: 4,
+        reviews: []
+    },
+    {
+        id: 1,
+        name: 'Adidas Tshirt',
+        decription: 'Adidas brand with certified material',
+        category: 'mobileAccesories',
+        image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
+        price: 178.00,
+        discount: 0,
+        isFavourite: false,
+        rank: 4,
+        reviews: []
+    },
+    {
+        id: 1,
+        name: 'Adidas Tshirt',
+        decription: 'Adidas brand with certified material',
+        category: 'mobileAccesories',
+        image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
+        price: 178.00,
+        discount: 0,
+        isFavourite: false,
+        rank: 4,
+        reviews: []
+    },
+    {
+        id: 1,
+        name: 'Adidas Tshirt',
+        decription: 'Adidas brand with certified material',
+        category: 'mobileAccesories',
+        image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
+        price: 178.00,
+        discount: 0,
+        isFavourite: false,
+        rank: 4,
+        reviews: []
+    },
+    {
+        id: 1,
+        name: 'Adidas Tshirt',
+        decription: 'Adidas brand with certified material',
+        category: 'mobileAccesories',
+        image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
+        price: 178.00,
+        discount: 0,
+        isFavourite: false,
+        rank: 4,
+        reviews: []
+    },
+    {
+        id: 1,
+        name: 'Adidas Tshirt',
+        decription: 'Adidas brand with certified material',
+        category: 'mobileAccesories',
+        image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
+        price: 178.00,
+        discount: 0,
+        isFavourite: false,
+        rank: 4,
+        reviews: []
+    },
+    {
+        id: 1,
+        name: 'Adidas Tshirt',
+        decription: 'Adidas brand with certified material',
+        category: 'mobileAccesories',
+        image: ['img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png', 'img/creativetribe.png'],
+        price: 178.00,
+        discount: 0,
         isFavourite: true,
         rank: 4,
         reviews: []
@@ -243,6 +411,35 @@ var arrProducts = [{
 
 
 $(document).ready(function() {
+
+    var wrap = $("#cont");
+    var el = $("#wrap");
+
+    wrap.on("scroll", function(e) {
+
+        if (this.scrollTop > 167) {
+            el.addClass("scroll-enable");
+        } else {
+            el.removeClass("scroll-enable");
+        }
+    });
+
+    var images = ["offer1.jpg", "offer2.png", "offer3.jpg", "offer4.jpg", "offer5.jpg", "offer6.jpg"];
+    $(function() {
+        var i = 0;
+        $("#slider").css("background-image", "url(img/" + images[i] + ")");
+        setInterval(function() {
+            i++;
+            if (i == images.length) {
+                i = 0;
+            }
+            $("#slider").fadeOut("0.5s", function() {
+                $(this).css("background-image", "url(img/" + images[i] + ")");
+                $(this).fadeIn("0.5s");
+            });
+        }, 5000);
+    });
+
     $(document).ready(function() {
         $("#btnViewCart").click(function() {
             alert('PRODUCT');
@@ -259,7 +456,7 @@ $(document).ready(function() {
                 "<span> <i class='fa fa-heart' aria-hidden='true'></i> </span>" +
                 "<span> <i class='fa fa-share-alt' aria-hidden='true'></i> </span>" +
                 "</div>" +
-                "<div class='card-content-2'>" +
+                "<div class='card-content-2 vertical'>" +
                 "<img src=" + item.image[0] + ">" +
                 "</div>" +
                 "<div class='card-content-3 vertical'>" +
@@ -278,7 +475,7 @@ $(document).ready(function() {
                 "<span> <i class='fa fa-heart-o' aria-hidden='true'></i> </span>" +
                 "<span> <i class='fa fa-share-alt' aria-hidden='true'></i> </span>" +
                 "</div>" +
-                "<div class='card-content-2'>" +
+                "<div class='card-content-2 vertical'>" +
                 "<img src=" + item.image[0] + ">" +
                 "</div>" +
                 "<div class='card-content-3 vertical'>" +
