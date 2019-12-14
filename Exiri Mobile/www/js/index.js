363350a68292a01a74e17f5ab4401baf554336da
mMushtharq <mushtharq.2015150@iit.ac.lk>
2019-12-14 17:27:50 +0530

    JSON Configs

--- Exiri Mobile/www/js/index.js
+++ Exiri Mobile/www/js/index.js
@@ -17,7 +17,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        window.location.href = window.location.href + 'login.html';
+        window.location.href = window.location.href + 'product.html';
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
