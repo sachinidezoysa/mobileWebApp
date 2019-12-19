f703c82c58c21f297fc7fb83fe011b83bc087f50
mbsundar <banusundar.2016144@iit.ac.lk>
2019-12-20 04:12:39 +0530

    dashboard

--- Exiri Mobile/www/js/dashboard.js
+++ Exiri Mobile/www/js/dashboard.js
@@ -1,17 +1,140 @@
 $(document).ready(function() {
+    var users = [{
+            name: 'Shifan',
+            password: '4584',
+            email: 'abc@gmail.com',
+            purchases: [{
+                    category: 'Electronics',
+                    price: 150.00,
+                },
+                {
+                    category: 'Fashion & Trend',
+                    price: 160.00,
+                }, {
+                    category: 'Mens Wear',
+                    price: 1650.00,
+                }, {
+                    category: 'Ladies Wear',
+                    price: 180.00,
+                }, {
+                    category: 'Gift & Soveniours',
+                    price: 190.00,
+                },
+                {
+                    category: 'Gift & Soveniours',
+                    price: 190.00,
+                }
+            ],
+            profileImage: 'img/user1.jpg'
+        },
+        {
+            name: 'sachini',
+            password: '4584',
+            email: 'abc@gmail.com',
+            purchases: [{
+                    category: 'Electronics',
+                    price: 150.00,
+                },
+                {
+                    category: 'Fashion & Trend',
+                    price: 160.00,
+                }, {
+                    category: 'Mens Wear',
+                    price: 170.00,
+                }, {
+                    category: 'Ladies Wear',
+                    price: 180.00,
+                }, {
+                    category: 'Gift & Soveniours',
+                    price: 190.00,
+                }
+            ],
+            profileImage: 'img/user2.jpg'
+        },
+        {
+            name: 'banu',
+            password: '4584',
+            email: 'abc@gmail.com',
+            purchases: [{
+                    category: 'Electronics',
+                    price: 150.00,
+                },
+                {
+                    category: 'Fashion & Trend',
+                    price: 160.00,
+                }, {
+                    category: 'Mens Wear',
+                    price: 170.00,
+                }, {
+                    category: 'Ladies Wear',
+                    price: 180.00,
+                }, {
+                    category: 'Gift & Soveniours',
+                    price: 190.00,
+                }
+            ],
+            profileImage: 'img/user3.jpg'
+        },
+        {
+            name: 'mushtharq',
+            password: '4584',
+            email: 'abc@gmail.com',
+            purchases: [{
+                    category: 'Electronics',
+                    price: 150.00,
+                },
+                {
+                    category: 'Fashion & Trend',
+                    price: 160.00,
+                }, {
+                    category: 'Mens Wear',
+                    price: 170.00,
+                }, {
+                    category: 'Ladies Wear',
+                    price: 180.00,
+                }, {
+                    category: 'Gift & Soveniours',
+                    price: 190.00,
+                }
+            ],
+            profileImage: 'img/user4.jpg'
+        }
+
+    ];
+
+
+    // CREATING LEADERBOARD
+    var mem = $('.members');
+    users.forEach((item) => {
+        var leader =
+            "<div class='horizontal'>" +
+            "<img src='" + item.profileImage + "'>" +
+            "<div class='vertical'>" +
+            "<a>" + item.name + "</a>" +
+            // "<div style='width:50%' class='progress-bar'> <span id=" + item.name + "> </span> </div>" +
+            "<div class='progress-bar'> <span style='width:" + (100 / item.purchases.length) * users.length + "%' id=" + item.name + "> </span> </div>" +
+            // "<div class='progress-bar'> <span style='width:80%' id=" + item.name + "> </span> </div>" +
+            "</div> </div>";
+        mem[0].innerHTML += leader;
+    });
+
+
+
+
+
     // PROMOTION SLIDER IMAGES
     var images = ["offer1.jpg", "offer2.png", "offer3.jpg", "offer4.jpg", "offer5.jpg", "offer6.jpg"];
-    var leaderBoard = {
-        shifan: 5,
-        banu: 7,
-        mushtharq: 8,
-        sachini: 9
-    };
+    // var leaderBoard = {
+    //     shifan: 5,
+    //     banu: 7,
+    //     mushtharq: 8,
+    //     sachini: 9
+    // };
 
-    $("#shifan").css('width', (((100 / leaderBoard.shifan) * Object.keys(leaderBoard).length) + "%"));
-    $("#sachini").css('width', (((100 / leaderBoard.sachini) * Object.keys(leaderBoard).length) + "%"));
-    $("#banu").css('width', (((100 / leaderBoard.banu) * Object.keys(leaderBoard).length) + "%"));
-    $("#mushtharq").css('width', (((100 / leaderBoard.mushtharq) * Object.keys(leaderBoard).length) + "%"));
+    // $("#shifan").css('width', (((100 / leaderBoard.shifan) * Object.keys(leaderBoard).length) + "%"));
+    // $("#sachini").css('width', (((100 / leaderBoard.sachini) * Object.keys(leaderBoard).length) + "%"));
+    // $("#banu").css('width', (((100 / leaderBoard.banu) * Object.keys(leaderBoard).length) + "%"));
+    // $("#mushtharq").css('width', (((100 / leaderBoard.mushtharq) * Object.keys(leaderBoard).length) + "%"));
 
     var options = {
         animationEnabled: true,
